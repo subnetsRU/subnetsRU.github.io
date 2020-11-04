6cdb4d13342af41689921724ef3b5e060475f69f
subnetsRU <root@subnets.ru>
2020-11-04 12:43:25 +0300

    add canvs poker for sberbox

---
+++ sb/poker/precache-manifest.7b271a9a1df7720aee0cc0faf295b3d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ad52942a963c00c711ec8e7d0a5b4e",
+    "url": "/sb/poker/index.html"
+  },
+  {
+    "revision": "6cfedf26e2e0c44e20ec",
+    "url": "/sb/poker/static/css/main.3f56fbeb.chunk.css"
+  },
+  {
+    "revision": "73a6696240f26ea7f031",
+    "url": "/sb/poker/static/js/2.ede909d4.chunk.js"
+  },
+  {
+    "revision": "6cfedf26e2e0c44e20ec",
+    "url": "/sb/poker/static/js/main.2f6976bb.chunk.js"
+  },
+  {
+    "revision": "accf9d97aade2dff7159",
+    "url": "/sb/poker/static/js/runtime-main.1fa6ad9a.js"
+  },
+  {
+    "revision": "c1ecfea003562ef849baa6ee05834601",
+    "url": "/sb/poker/static/media/poker_stol.alice.2.c1ecfea0.PNG"
+  },
+  {
+    "revision": "45397b250d0b3049ac6788fa68ab448f",
+    "url": "/sb/poker/static/media/sphere.45397b25.png"
+  }
+]);
